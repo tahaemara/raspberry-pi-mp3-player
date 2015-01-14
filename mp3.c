89eb3d192e9f5bee81cb1d2ab34f4049791952ba
Taha Emara <tahaemarah@yahoo.com>
2015-01-14 17:47:41 +0200

    Update mp3.c

--- mp3.c
+++ mp3.c
@@ -25,9 +25,22 @@
 //  to make program run on startup :
 //   type in terminal "sudo nano /etc/rc.local" 
 //   and add this line to this file " sudo /path/to/folder/mp3 & "  in my case " sudo /home/pi/mp3player/mp3 &"
+//////////////////////////////////////////////  Licence ////////////////////////////////////////////////////////
 //
-//   Free for non commercial use as long as this entire copyright notice is included or not in source code 
-//    and any other documentation .
+//    This file is part of Foobar.
+//
+//  Foobar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Foobar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Foobar.  If not, see <http://www.gnu.org/licenses/.
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
